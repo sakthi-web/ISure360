b61127791c8a37c0a8307c44e5ff95a52681b3e8
Sakthivel <sakthi.pnk@gmail.com>
2023-05-19 18:52:47 +0530

    Add files via upload

---
+++ src/routes.js
@@ -0,0 +1,62 @@
+// import
+import Dashboard from "views/Dashboard/Dashboard";
+import Tables from "views/Dashboard/Tables";
+import SignIn from "views/Auth/SignIn.js";
+import SignUp from "views/Auth/SignUp.js";
+
+import {
+  HomeIcon,
+  StatsIcon,
+  CreditIcon,
+  PersonIcon,
+  DocumentIcon,
+  RocketIcon,
+  SupportIcon,
+} from "components/Icons/Icons";
+
+var dashRoutes = [
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    rtlName: "لوحة القيادة",
+    icon: <HomeIcon color="inherit" />,
+    component: Dashboard,
+    layout: "/admin",
+    protected: true,
+  },
+  {
+    path: "/tables",
+    name: "Tables",
+    rtlName: "لوحة القيادة",
+    icon: <StatsIcon color="inherit" />,
+    component: Tables,
+    layout: "/admin",
+    protected: true,
+  },
+  {
+    name: "ACCOUNT PAGES",
+    category: "account",
+    rtlName: "صفحات",
+    state: "pageCollapse",
+    views: [
+      {
+        path: "/signin",
+        name: "Sign In",
+        rtlName: "لوحة القيادة",
+        icon: <DocumentIcon color="inherit" />,
+        component: SignIn,
+        layout: "/auth",
+      },
+      {
+        path: "/signup",
+        name: "Sign Up",
+        rtlName: "لوحة القيادة",
+        icon: <RocketIcon color="inherit" />,
+        secondaryNavbar: true,
+        component: SignUp,
+        layout: "/auth",
+      },
+    ],
+  },
+];
+export default dashRoutes;
