d97cb06adcd878aa7b88c34482a0a3e9978f682c
Sakthivel <sakthi.pnk@gmail.com>
2023-05-29 17:32:24 +0530

    Added Table Component

--- src/views/Dashboard/Tables/index.js
+++ src/views/Dashboard/Tables/index.js
@@ -1,12 +1,78 @@
 // Chakra imports
-import { Flex } from "@chakra-ui/react";
+import {
+  Button,
+  ButtonGroup,
+  Flex,
+  IconButton,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  useColorModeValue,
+} from "@chakra-ui/react";
 import React from "react";
 import PieRechartComponent from "components/Charts/PieChart";
+import DescriptionTable from "components/Tables/DescriptionTable";
+import { SearchIcon } from "@chakra-ui/icons";
+
 function Tables() {
+  let mainTeal = useColorModeValue("teal.300", "teal.300");
+  let inputBg = useColorModeValue("white", "gray.800");
+  let mainText = useColorModeValue("gray.700", "gray.200");
+  let searchIcon = useColorModeValue("gray.700", "gray.200");
   return (
-    <Flex direction="column" pt={{ base: "120px", md: "75px" }}>
-      <PieRechartComponent />
-    </Flex>
+    <>
+      <Flex alignItems="center" pt={{ base: "120px", md: "75px" }} mt="1rem">
+        {/* <PieRechartComponent /> */}
+        <InputGroup
+          cursor="pointer"
+          bg={inputBg}
+          borderRadius="15px"
+          w={{
+            sm: "128px",
+            md: "200px",
+          }}
+          me={{ sm: "auto", md: "20px" }}
+          ml="auto"
+          _focus={{
+            borderColor: { mainTeal },
+          }}
+          _active={{
+            borderColor: { mainTeal },
+          }}
+        >
+          <InputLeftElement
+            children={
+              <IconButton
+                bg="inherit"
+                borderRadius="inherit"
+                _hover="none"
+                _active={{
+                  bg: "inherit",
+                  transform: "none",
+                  borderColor: "transparent",
+                }}
+                _focus={{
+                  boxShadow: "none",
+                }}
+                icon={<SearchIcon color={searchIcon} w="15px" h="15px" />}
+              ></IconButton>
+            }
+          />
+          <Input
+            fontSize="xs"
+            py="11px"
+            color={mainText}
+            placeholder="Search..."
+            borderRadius="inherit"
+          />
+        </InputGroup>
+        <ButtonGroup size="sm">
+          <Button>Refresh Ranking</Button>
+          <Button>Associate Agent Work</Button>
+        </ButtonGroup>
+      </Flex>
+      <DescriptionTable />
+    </>
   );
 }
 
